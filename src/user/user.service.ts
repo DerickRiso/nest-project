572bee4a543d6631d6ad318929ac371d3764b028
Derick Darlan <derickdarlan304@gmail.com>
2025-06-24 11:02:12 -0300

    CRUD concluído

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { PrismaService } from "src/prisma/prisma.service";
 import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
@@ -34,6 +34,8 @@ export class UserService {
 
     async update(id: number, data: UpdatePutUserDTO) {
 
+        await this.exists(id);
+
         return this.prisma.user.update({
             data,
             where: {
@@ -43,11 +45,31 @@ export class UserService {
     }
 
     async updatePartial(id: number, data: UpdatePatchUserDTO) {
+
+        await this.exists(id);
+
         return this.prisma.user.update({
             data,
             where: {
                 id
             }
         })
+    }
+
+    async delete(id: number) {
+
+        await this.exists(id);
+
+        return this.prisma.user.delete({
+            where: {
+                id
+            }
+        })
+    }
+
+    async exists(id: number) {
+        if(!await this.show(id)) {
+            throw new NotFoundException(`O usuário ${id} não existe`);
+        }
     }
 }
